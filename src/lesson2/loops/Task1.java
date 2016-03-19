ffc6134432341df6abc1ff1b8bc59ba777cec0b2
MykhayloKhapchyk <micha_dom_228@mail.ru>
2016-03-19 10:09:14 +0200

    Update Task1.java

--- src/lesson2/loops/Task1.java
+++ src/lesson2/loops/Task1.java
@@ -2,6 +2,8 @@ package lesson2.loops;
 
 /**
  * Created by Михаил on 05.03.2016.
+ * Вывести на экран числа от 10 до 20 с помощью всех известных циклов.
+ * 
  */
 public class Task1 {
     public static void main(String [] args){
